2976ca36cc539c62d071739ccc4a761a0ee46878
Ashok Gelal <ashokgelal@gmail.com>
2024-07-02 23:05:03 -0400

    init

---
+++ app.config.ts
@@ -0,0 +1,63 @@
+export default defineAppConfig({
+  shadcnDocs: {
+    site: {
+      name: 'shadcn-docs-nuxt',
+      description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
+    },
+    theme: {
+      customizable: true,
+      color: 'blue',
+      radius: 0.5,
+    },
+    header: {
+      title: 'shadcn-docs-starter',
+      showTitle: true,
+      darkModeToggle: true,
+      logo: {
+        light: '/logo.svg',
+        dark: '/logo-dark.svg',
+      },
+      nav: [],
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    aside: {
+      useLevel: true,
+      collapse: false,
+    },
+    main: {
+      breadCrumb: true,
+      showTitle: true,
+    },
+    footer: {
+      credits: 'Copyright © 2024',
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    toc: {
+      enable: true,
+      title: 'On This Page',
+      links: [{
+        title: 'Star on GitHub',
+        icon: 'lucide:star',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }, {
+        title: 'Create Issues',
+        icon: 'lucide:circle-dot',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt/issues',
+        target: '_blank',
+      }],
+    },
+    search: {
+      enable: true,
+      inAside: false,
+    }
+  }
+});
---
+++ content/index.md
@@ -0,0 +1,30 @@
+---
+title: Home
+navigation: false
+---
+
+::hero
+---
+announcement:
+  title: 'Release v0.3.1'
+  icon: 'noto:party-popper'
+  to: /getting-started
+actions:
+  - name: Get Started
+    to: /getting-started
+  - name: GitHub
+    variant: outline
+    to: https://github.com/ZTL-UwU/shadcn-docs-nuxt
+    leftIcon: 'lucide:github'
+---
+
+#title
+Msty Docs
+
+#description
+Beautifully designed Nuxt Content template with shadcn-vue.<br>Customizable. Compatible. Open Source.
+::
+
+<div class="border rounded-lg shadow-md">
+  <iframe src="/getting-started/writing/components" height="700" width="100%" class="rounded-lg" scrolling="no" frameborder="0">
+</div>
