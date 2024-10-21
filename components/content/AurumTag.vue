727c3ee9c1a03dcd65d35c356200388c4a03df51
Nikesh Parajuli <parajulinikesh@gmail.com>
2024-10-20 22:46:28 -0400

    tweak: aurum tag

--- components/content/AurumTag.vue
+++ components/content/AurumTag.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="mx-0.5">
-    <div
-      class="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold border-transparent bg-yellow-400 dark:text-secondary text-secondary-foreground gap-1"
-    >
-      <Icon name="tabler:sparkles" />
-      <span>Aurum Perk</span>
-    </div>
+  <div
+    class="align-middle mx-0.5 inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold border-transparent bg-yellow-400 dark:text-secondary text-secondary-foreground gap-1"
+  >
+    <Icon name="tabler:sparkles" />
+    <span>Aurum Perk</span>
   </div>
 </template>
